f85493f585bfc79827dea54eccd1fb87cfae92aa
Mezyann TELLAL <mtellal@paul-f3Ar3s4.clusters.42paris.fr>
2024-03-15 17:42:17 +0100

    norme ex06 - ex07 in progress

--- day00/ex06/filterstring.py
+++ day00/ex06/filterstring.py
@@ -1,32 +1,47 @@
 import sys
+from ft_filter import ft_filter
+
+"""
+
+This program take 2 arguments a string(S) and an integer(N)
+and display the number of words with more than N.
+
+if the number is invalid the programm must display an AssertionError
+
+"""
+
 
 def verify_arguments() -> int:
     len_args = len(sys.argv)
     assert len_args == 3, "the arguments are bad"
     try:
         s = str(sys.argv[2])
-        sign = 1;
+        sign = 1
         if s[0] == '-' or s[0] == '+':
             if s[0] == '-':
                 sign *= -1
             s = s[1:]
-        if s.isdigit() == False:
+        if s.isdigit() is False:
             raise Exception("not valid int")
         else:
             return int(s) * sign
-    except:
+    except Exception:
         raise AssertionError("the arguments are bad")
+
 
 def main():
     try:
         final = []
-        word_len  = verify_arguments()
+        word_len = verify_arguments()
         words = sys.argv[1].split(" ")
-        valid_word = lambda x: (len(x) > word_len and len(x) > 0)
-        final = [x for x in words if valid_word(x)]
+        it = ft_filter(lambda x: (len(x) > word_len and len(x) > 0), words)
+        final = []
+        for x in it:
+            final.append(x)
         print(final)
     except AssertionError as msg:
         print("AssertionError:", msg)
+
 
 if __name__ == "__main__":
     main()
--- day00/ex06/ft_filter.py
+++ day00/ex06/ft_filter.py
@@ -6,7 +6,8 @@ Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.
 """
 
+
 def ft_filter(function, iterable):
-    if (type(function) == type(None)):
+    if function is None:
         return iter(iterable)
     return iter(x for x in iterable if function(x))
