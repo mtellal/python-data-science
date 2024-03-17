09e82508cb2b4fa7b0dbcb5c232ff7d5a9b39e8a
mtellal <mez.tellal@gmail.com>
2024-03-17 20:16:24 +0100

    day01 - ex00

--- day01/ex00/give_bmi.py
+++ day01/ex00/give_bmi.py
@@ -13,9 +13,16 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
         assert type(height) is list and type(weight) is list, "bad type list"
         verify_lists(height, weight)
         print("list valid, going on ...")
+        final = [None] * len(height)
+        for i in range(0, len(height)):
+            final[i] = weight[i] / (height[i] ** 2)
+        return final
     except AssertionError as msg:
         print("Error:", msg)
 
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    return list(map(lambda x: x > limit, bmi))
 
 def main():
     give_bmi(["dw", 1, 2, 12.3], [])
