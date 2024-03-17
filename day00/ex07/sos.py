6c4600c52c693103008dfbff194a4dafcf90f6e5
mtellal <mez.tellal@gmail.com>
2024-03-17 20:01:10 +0100

    day00 - ex07

--- day00/ex07/sos.py
+++ day00/ex07/sos.py
@@ -1,6 +1,22 @@
+import sys
 
 
-def main():
+"""
+    This program convert a string argument in morse.
+
+    Args:
+        s (str): string to convert
+
+    Returns:
+        print the converted string in stdout
+"""
+
+
+def verify_text():
+    assert len(sys.argv) == 2, "the arguments are bad"
+
+
+def convertChar(c: str) -> str:
     NESTED_MORSE = {
             'A': '.-',
             'B': '-...',
@@ -38,9 +54,26 @@ def main():
             '7': '--...',
             '8': '---..',
             '9': '----.',
-            ' ': ' '
     }
+    return NESTED_MORSE[c.upper()] + " "
 
+
+def main():
+    try:
+        verify_text()
+        text = sys.argv[1]
+        final = ""
+        for x in text:
+            if x.isalnum() is False:
+                if x.isspace():
+                    final += "\\" + " "
+                else:
+                    raise AssertionError("the arguments are bad")
+            else:
+                final += convertChar(x)
+        print(final[:-1])
+    except AssertionError as msg:
+        print("AssertionError:", msg)
 
 if __name__ == "__main__":
     main()
